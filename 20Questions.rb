6a326e504746f06ee913d598b77eacbad35a0a91
Rooth <roothb@gmail.com>
2014-04-21 18:28:04 -0400

    20 Questions

--- 20Questions.rb
+++ 20Questions.rb
@@ -1,7 +1,33 @@
-puts 'Please enter Yes or No to each of the following questions.'
-puts 'Does Hank Aaron holds the records for most home runs in a career?'
-if Yes
-	puts '1'
+#writing a 20 questions script
+# script should be run from the command line
+# script should ask the user a question, take in the response of true or false from the user
+# script should tell user if response was correct or not
+# after all questions and responses, script should output the "tally" or total correct
+
+
+
+# questions
+# correct answer
+# score
+
+#ask question
+puts "Is the sky (mostly) blue"
+
+# read in from user
+user_answer = gets.chomp
+
+#compare the user response to known good response
+user_answer == 'y'
+
+#output if user is correct or not
+if user_answer == 'y'
+	#output if the user answer is correct or not
+	puts "Well done, correct"
+elsif user_answer == 'n'
+	puts "try harder, wrong"
 else
-	puts '0'
+	puts "is not legible"
 end
+
+#update our score based on the response
+
