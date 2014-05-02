c1ae5f5d31b17941bdd0748398d30d9219cd7b5c
Rooth <roothb@gmail.com>
2014-05-01 22:28:26 -0400

    May 1 Update

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,6 +1,5 @@
 class GalleryController < ApplicationController
   def index
-  	session['cat'] = robby
   	@image_names = ["parentaladvisory", "partridgefamily", "someband", "someotherband"]
   end
 end
