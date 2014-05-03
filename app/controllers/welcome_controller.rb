578c119a49cf7978e22a542ad82c300c27b2fd59
Rooth <roothb@gmail.com>
2014-05-03 17:08:58 -0400

    May 3 V2

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
-  
   def say_hi
   	@say_hi_name = params['my_name']
-  end
+  	end
+
   def say_hi_password
-  	@say_hi_password = params['my_password']
-  end
+  	@say_hi_name_password = params['my_password']
+  	end
 end
