7849badf8bd7d40492257e1f623bf78aedda15d8
Rooth <roothb@gmail.com>
2014-05-11 18:23:01 -0400

    May11

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,9 +1,16 @@
 class AdminController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :admin_user
 
   def Private
   end
 
   def TopSecret
+  end
+end
+
+  def admin_user
+    if current_user.admin != true
+      redirect_to(welcome_page_path, alert: "You're not an admin")
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,15 +9,13 @@ class WelcomeController < ApplicationController
   end
 
   def say_hi
-  	@say_hi_name = params['my_name']
-  	@say_hi_name_password = params['my_password']
-  	right_password = 'Right'
-
-  	if @say_hi_name_password == right_password
-  		session['admin'] == true
-  	else
-  		session['admin'] == false
-  	end
+  	@say_hi_password = params['my_password']
+  	right_password = '1967Dart'
+  	if @say_hi_password == right_password
+      puts "You're in like an Admin!"
+  		current_user.admin = true
+      current_user.save
+    end
+    redirect_to(welcome_page_path)
   end
 end
-
