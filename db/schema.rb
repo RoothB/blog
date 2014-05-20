ef7b32bef18692f08e0f81bfb22d17eef411dc30
Rooth <roothb@gmail.com>
2014-05-19 20:43:43 -0400

    May 19-9PM

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140515005937) do
+ActiveRecord::Schema.define(version: 20140519235615) do
 
   create_table "images", force: true do |t|
     t.string   "name"
@@ -35,6 +35,10 @@ ActiveRecord::Schema.define(version: 20140515005937) do
     t.string   "file_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "themes", force: true do |t|
